e2f7499de3aaff3073f5b41e342b2a7fe6dfc85c
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-19 17:58:32 +0300

    Add delete functionality to task model

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
 
-from todo.views import TaskListView, TaskCreateView, TaskUpdateView
+from todo.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
-    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path(
+        "task/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update",
+    ),
+    path(
+        "task/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete",
+    ),
 ]
 
 app_name = "todo"
