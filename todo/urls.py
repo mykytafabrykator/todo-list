cd1a10cc877b8139444b952e26205409eec75d1e
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-19 17:49:49 +0300

    Add creation possibility to task

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from todo.views import TaskListView
+from todo.views import TaskListView, TaskCreateView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from todo.forms import TaskForm
 from todo.models import Task
 
 
 # Create your views here.
 class TaskListView(generic.ListView):
     model = Task
+    queryset = Task.objects.all().order_by(
+        "is_completed",
+        "-creation_datetime",
+    )
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
